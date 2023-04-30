d17bb33c5db92880b98e15f9a8ca590586637f41
Somnie Maxim Belkovsky <inkerimaa1@gmail.com>
2023-04-30 14:33:57 +0300

    base asset

--- midilib/src/main/cpp/instrument/AssetInstrument.h
+++ midilib/src/main/cpp/instrument/AssetInstrument.h
@@ -11,13 +11,19 @@ public:
     using Instrument::Instrument;
 
     void loadAsset(int8_t note, unique_ptr<vector<uint8_t>> wavData);
+    void loadBaseAsset(int8_t baseNote, unique_ptr<vector<uint8_t>> wavData);
     void setRepeatable(bool isRepeatable);
 
     float sample(double time, int8_t note) override;
 
+    static void setResamplingQuality(uint8_t quality);
+
 private:
     vector<vector<float>> mSamples = vector<vector<float>>(SCHAR_MAX + 1);
     bool mIsRepeatable = false;
+    static uint8_t mResamplingQuality;
+
+    void resampleAndAssign(vector<float>& dataIn, double ratio, int8_t note);
 };
 
 
--- midilib/src/main/java/com/example/midilib/instrument/AssetInstrument.kt
+++ midilib/src/main/java/com/example/midilib/instrument/AssetInstrument.kt
@@ -15,18 +15,33 @@ open class AssetInstrument(
     var repeatAssets = repeatAssets
         set(value) {
             field = value
-            if (libIndex != NO_INDEX) setRepeatable(value)
+            if (libIndex != NO_INDEX) externalSetRepeatable(value)
         }
 
-    fun loadAsset(context: Context, note: Byte, filename: String) {
+    fun loadAsset(context: Context, note: Byte, filename: String, isBaseAsset: Boolean = false) {
         val stream = context.assets.open(filename)
         val bytes = stream.readBytes()
-        externalLoadAsset(note, bytes, bytes.size)
+        externalLoadAsset(note, bytes, bytes.size, isBaseAsset)
         stream.close()
     }
 
-    private external fun externalLoadAsset(note: Byte, wavData: ByteArray, dataSize: Int)
-    private external fun setRepeatable(repeatable: Boolean)
+    private external fun externalLoadAsset(note: Byte, wavData: ByteArray, dataSize: Int, isBaseAsset: Boolean)
+    private external fun externalSetRepeatable(repeatable: Boolean)
 
     override fun clone() = AssetInstrument(attack, decay, sustain, release)
+
+    enum class ResamplingQuality {
+        SINC_BEST_QUALITY,
+        SINC_MEDIUM_QUALITY,
+        SINC_FASTEST,
+        ZERO_ORDER_HOLD,
+        LINEAR,
+    }
+
+    companion object {
+        fun setResamplingQuality(quality: ResamplingQuality) {
+            externalSetResamplingQuality(quality.ordinal);
+        }
+        private external fun externalSetResamplingQuality(quality: Int)
+    }
 }
